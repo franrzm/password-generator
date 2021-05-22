634d2073bf56653e3f144b5aa496302a6b9f67ce
franrzm <franciscoruiz5398@gmail.com>
2021-05-22 16:56:18 +0200

    feat: :rocket: first commit
    up and runnig

---
+++ go.mod
@@ -0,0 +1,5 @@
+module password-generator
+
+go 1.13
+
+require github.com/atotto/clipboard v0.1.4
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"math/rand"
+	"time"
+
+	"github.com/atotto/clipboard"
+)
+
+var defaultLenght int = 30
+
+var (
+	lowerCaseLetters = []byte("abcdefghijklmnopqrstuvwxyz")
+	upperCaseLetters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	numbers          = []byte("0123456789")
+	symbols          = []byte("`~!@#$%^&*()_-+={}[]\\|:;\"'<>,.?/")
+)
+
+func generatePassword(stringLength int) string {
+	randomString := make([]byte, stringLength)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range randomString {
+		switch rand.Intn(4) {
+		case 0:
+			randomString[i] = lowerCaseLetters[rand.Intn(len(lowerCaseLetters))]
+		case 1:
+			randomString[i] = upperCaseLetters[rand.Intn(len(upperCaseLetters))]
+		case 2:
+			randomString[i] = numbers[rand.Intn(len(numbers))]
+		case 3:
+			randomString[i] = symbols[rand.Intn(len(symbols))]
+		}
+	}
+
+	return string(randomString)
+}
+
+func main() {
+	var passwordLength int
+	var showPassword bool
+
+	flag.IntVar(&passwordLength, "l", defaultLenght, "Specify the length of the password. Default length is 30.")
+	flag.BoolVar(&showPassword, "s", false, "Holi")
+
+	flag.Parse()
+
+	var password string = generatePassword(passwordLength)
+
+	err := clipboard.WriteAll(password)
+
+	if err != nil {
+		panic(err)
+	} else {
+		println("The password has been copied to clipboard!")
+		println("Go to paste it!")
+	}
+
+	if showPassword {
+		println("Password: ", password)
+	}
+}
